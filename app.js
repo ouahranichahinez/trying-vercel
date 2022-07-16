0acbd08bc2c4f367de8638fb256a338e85cba972
cchahinez <chahinez.work@gmail.com>
2022-07-16 20:27:31 +0100

    p

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import express from "express";
 import { bot } from "./api/bot.js";
 
 export const app = express();
-app.get("/", async (req, res) => {
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+/*app.get("/", async (req, res) => {
     await bot();
     res.send("you are finished, see you next time :) .");
 });
-
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
+*/
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
 });
